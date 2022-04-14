3e3c8ed82ab79c6298cada91cdb59acc6888be0c
Sam <sfd9139@g.rit.edu>
2022-04-14 17:21:48 -0400

    found source of a major error

--- client/helper.js
+++ client/helper.js
@@ -1,6 +1,6 @@
 const handleError = (message) => {
   document.getElementById('errorMessage').textContent = message;
-  document.getElementById('domoMessage').classList.remove('hidden');
+  document.getElementById('animeMessage').classList.remove('hidden');
 };
 const sendPost = async (url, data, handler) => {
   const response = await fetch(url, {
@@ -11,7 +11,7 @@ const sendPost = async (url, data, handler) => {
     body: JSON.stringify(data),
   });
   const result = await response.json();
-  document.getElementById('domoMessage').classList.add('hidden');
+  document.getElementById('animeMessage').classList.add('hidden');
   if(result.error) {
     handleError(result.error);
   }
@@ -23,7 +23,7 @@ const sendPost = async (url, data, handler) => {
   }
 };
 const hideError=()=>{
-  document.getElementById('domoMessage').classList.add('hidden');
+  document.getElementById('animeMessage').classList.add('hidden');
 };
 module.exports={
   handleError,
